994ba31892a02d15d593ec54d8f8b02d6cd766c8
Anuj Deshpande <anujdeshpande92@gmail.com>
2015-02-13 23:49:47 +0000

    Only A works

--- harp.py
+++ harp.py
@@ -5,18 +5,23 @@ previnputs = [False for a in range(0, numpins)]
 
 if True:
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    ser1= serial.Serial('/dev/ttyACM2', 9600)
+    ser1= serial.Serial('/dev/ttyACM1', 9600)
 
 pygame.mixer.pre_init(channels=6, buffer=1024)
 pygame.mixer.init()
 
-guit_let = ["a", "b", "b1", "c", "c1", "d","e", "e1", "f", "f1", "g","g1"]
+guit_let = ["olda", "b", "b1", "c", "c1", "d","e", "e1", "f", "f1", "g","g1"]
 guitar_notes = [pygame.mixer.Sound("newsounds/"+letter+".wav") for letter in guit_let]
 
 
 def guitar(i):
-    guitar_notes[i].play()
-
+    if i==0:
+	    guitar_notes[i].play()
+#	    time.sleep(4)
+	    print guit_let[i]
+	    for j in [2,11]:
+		guitar_notes[j].play()
+		time.sleep(0.4)
 count = 0
 
 while True:
@@ -24,12 +29,13 @@ while True:
     line = ""
     if True:
         line = ser.readline()
-        print line
+#        print line
+
     else:
         line = raw_input()
-    line += ser1.readline() 
-    print line
 
+    line += ser1.readline() 
+#    print line
     for i in range(0, numpins):
         curr = line[i] != '0'
         prev = previnputs[i]
