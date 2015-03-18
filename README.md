230f4237e6c91082bc9b86d67ba42991f821d9ba
rituparna89 <rituparna89@gmail.com>
2015-03-18 15:59:33 +0000

    first commit

--- README.md
+++
@@ -1,8 +0,0 @@
-piano-stairs
-============
-
-A project for HackPrinceton Fall 2013. Demo'd on Nov 10th, 2013.
-
-Blog post:
-
-bonnie-eisenman.tumblr.com/post/66630942173/hackprinceton-fall-2013-piano-stairs
--- harp.py
+++ harp.py
@@ -1,6 +1,9 @@
+# LASER harp
+# Author :Rituparna Matkar
+# This script reads the inputs from the serial port and play notes accordingly
 import pygame, serial, time
 
-numpins = 12
+numpins = 11
 previnputs = [False for a in range(0, numpins)]
 
 if True:
@@ -15,13 +18,8 @@ guitar_notes = [pygame.mixer.Sound("newsounds/"+letter+".wav") for letter in gui
 
 
 def guitar(i):
-    if i==0:
-	    guitar_notes[i].play()
-#	    time.sleep(4)
-	    print guit_let[i]
-	    for j in [2,11]:
-		guitar_notes[j].play()
-		time.sleep(0.4)
+	guitar_notes[i].play()
+	print guit_let[i]
 count = 0
 
 while True:
@@ -29,17 +27,13 @@ while True:
     line = ""
     if True:
         line = ser.readline()
-#        print line
-
     else:
         line = raw_input()
     line = line.strip('\n')
     line += ser1.readline() 
-#    print line
     for i in range(0, numpins):
         curr = line[i] != '0'
         prev = previnputs[i]
         if curr and not prev:
             guitar(i)
         previnputs[i] = curr
-
